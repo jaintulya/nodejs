f17d2add1f1ee8ca087eb07eed7067006ab465ec
jaintulya <tulya.jain.cg@gmail.com>
2026-02-17 22:49:39 +0530

    17/2/26 noteapp api

--- yt/note-app/src/app.js
+++ yt/note-app/src/app.js
@@ -1,7 +1,60 @@
 const express = require('express')
 const app =express()
+app.use(express.json())
+
+const notes=[]
+
+app.post('/notes',(req,res)=>{
+   notes.push(req.body)
+   res.status(201).json({
+    message:"note created successfully"
+   })
+})
+ 
+app.get('/notes',(req,res)=>{
+  res.status(200).json({
+    message : "note fetched successfully",
+    notes: notes
+
+  })
+
+  
+})
+
+app.delete('/notes/:index',(req,res)=>{
+  const index=req.params.index
+
+  delete notes[index]
+
+  res.status(200).json({
+    message:"message deleted succesfully"
+  })
+})
+
+app.patch('/notes/:index',(req,res)=>{
+
+const index=req.params.index;
+const description=req.body.description
+const title=req.body.title
+
+
+notes[index].description= description
+
+notes[index].title=title
+res.status(200).json({
+  message: "note updated successfully"
+})
+
+
+})
+
+
+
+
 
 
 
 
 module.exports = app
+
+
