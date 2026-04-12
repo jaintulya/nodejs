cd3e0e30f6011b71f042b3c02e79d24900582d8d
jaintulya <tulya.jain.cg@gmail.com>
2026-04-12 15:42:48 +0530

    mongodb connected to server

--- yt/mongodb/src/app.js
+++ yt/mongodb/src/app.js
@@ -1,5 +1,32 @@
 const express= require('express');
-
+const noteModel=require("./models/note.model")
 const app =express();
+app.use(express.json());
+
+
+app.post("/notes", async(req,res)=>{
+
+  const data = req.body;
+  await noteModel.create({
+    title:data.title,
+    description:data.description
+  })
+
+  res.status(201).json({
+    message:"note created"
+  })
+})
+
+app.get('/notes', async (req,res)=>{
+ 
+ const notes=await noteModel.findOne({
+  title:"test_title"
+ })
+ res.status(200).json({
+  message:"notes fetched successfully",
+  notes:notes
+ })
+})
+
 
 module.exports = app
--- yt/mongodb/src/db/db.js
+++ yt/mongodb/src/db/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-async function  connectDB(){
+async function connectDB(){
   await mongoose.connect("mongodb+srv://tulya:AWtQxD5V8cGct0BT@cluster0.7hysmwm.mongodb.net/learning")
 
   console.log("Connected to DB")
 }
-module.export=connectDB
+module.exports= connectDB
 
